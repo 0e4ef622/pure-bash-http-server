19e3a1929ac22e6152ac469b5c3f7628633656d7
Matthew Tran <0e4ef622@gmail.com>
2017-07-22 13:28:26 -0500

    add KEEPALIVE_TIMEOUT option

--- run.sh
+++ run.sh
@@ -3,21 +3,21 @@
 PORT=80 # default port
 rootdir="./rootdir";
 error_404_page="/404.html"; # inside ./rootdir
+keepalive_timeout=10;
 
 while read line; do
     line=${line%%#*}; # strip comments
     read line <<< "$line"; # trim whitespace
-    if [ "${line%%=*}" = "PORT" ]; then
-        PORT=${line:5};
-    elif [ "${line%%=*}" = "ROOTDIR" ]; then
-        rootdir=${line:8};
-    elif [ "${line%%=*}" = "404_PAGE" ]; then
-        error_404_page=${line:9};
-    fi
+    case "${line%%=*}" in
+        PORT) PORT=${line:5};;
+        ROOTDIR) rootdir=${line:8};;
+        404_PAGE) error_404_page=${line:9};;
+        KEEPALIVE_TIMEOUT) keepalive_timeout=${line:18};;
+    esac
 done < options
 
 echo "Running server on port $PORT";
 
 export rootdir;
 export error_404_page;
-socat TCP4-LISTEN:$PORT,pktinfo,fork EXEC:./server.sh
+socat -T$keepalive_timeout TCP4-LISTEN:$PORT,pktinfo,fork EXEC:./server.sh
